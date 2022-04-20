334d162826f9789350940cfe6266d602f76c80ff
TrekkCode <79606472+TrekkCode@users.noreply.github.com>
2022-04-20 13:21:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -484,7 +484,7 @@ map.on('load', () => {
   }
 });
 
-// Modal - popup for filtering results
+/*// Modal - popup for filtering results
 const filterResults = document.getElementById('filterResults');
 const exitButton = document.getElementById('exitButton');
 const modal = document.getElementById('modal');
@@ -496,7 +496,7 @@ filterResults.addEventListener('click', () => {
 
 exitButton.addEventListener('click', () => {
   modal.classList.add('hide-visually');
-});
+});*/
 
 const title = document.getElementById('title');
 title.innerText = config.title;
